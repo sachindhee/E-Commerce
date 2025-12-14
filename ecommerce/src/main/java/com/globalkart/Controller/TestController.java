df5842db0d34655171acc0ef9afe102b5897cade
sachindhee <154836258+sachindhee@users.noreply.github.com>
2025-12-15 00:42:01 +0530

    add some file

---
+++ ecommerce/src/main/java/com/globalkart/Controller/TestController.java
@@ -0,0 +1,16 @@
+package com.globalkart.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String test(){
+        return "E-Commerce Backend Renning !";
+    }
+}
