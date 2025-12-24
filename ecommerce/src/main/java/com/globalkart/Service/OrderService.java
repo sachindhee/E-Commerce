4c246dfee98707d626276affbf87e9674535a70e
sachindhee <154836258+sachindhee@users.noreply.github.com>
2025-12-24 23:36:10 +0530

    Order service

---
+++ ecommerce/src/main/java/com/globalkart/Service/OrderService.java
@@ -0,0 +1,75 @@
+package com.globalkart.Service;
+
+import com.globalkart.Repository.CartRepository;
+import com.globalkart.Repository.OrderRepository;
+import com.globalkart.Repository.UserRepository;
+import com.globalkart.model.*;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final UserRepository userRepository;
+    private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
+
+    public OrderService(UserRepository userRepository,
+                        CartRepository cartRepository,
+                        OrderRepository orderRepository) {
+        this.userRepository = userRepository;
+        this.cartRepository = cartRepository;
+        this.orderRepository = orderRepository;
+    }
+
+
+
+    public Order placeOrder(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("User not found"));
+        Cart cart = cartRepository.findByUser(user)
+                .orElseThrow(() -> new RuntimeException("Cart not found"));
+
+        if (cart.getItems().isEmpty()) {
+            throw new RuntimeException("Cart is empty");
+        }
+
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setStatus("PLACED");
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        BigDecimal totalAmount = BigDecimal.ZERO;
+
+        for (CartItem cartItem : cart.getItems()) {
+            OrderItem item = new OrderItem();
+            item.setOrder(order);
+            item.setProduct(cartItem.getProduct());
+            item.setQuantity(cartItem.getQuantity());
+            item.setPrice(cartItem.getPrice());
+
+            orderItems.add(item);
+
+            totalAmount = totalAmount.add(
+                    cartItem.getPrice()
+                            .multiply(BigDecimal.valueOf(cartItem.getQuantity()))
+            );
+        }
+
+        order.setItems(orderItems);
+        order.setTotalAmount(totalAmount);
+
+        Order savedOrder = orderRepository.save(order);
+
+        // ✅ Clear cart after order
+        cart.getItems().clear();
+        cartRepository.save(cart);
+
+        return savedOrder;
+    }
+
+
+}
